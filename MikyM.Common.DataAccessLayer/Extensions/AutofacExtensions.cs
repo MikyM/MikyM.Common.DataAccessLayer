08ed696de47d879a639315a3a51cb8e8a7078d3f
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-08-15 15:40:17 +0200

    fix: autofac extensions

--- MikyM.Common.DataAccessLayer/Extensions/AutofacExtensions.cs
+++ MikyM.Common.DataAccessLayer/Extensions/AutofacExtensions.cs
@@ -16,6 +16,19 @@ namespace MikyM.Common.DataAccessLayer.Extensions
         /// <remarks>
         /// This method only support one db context, if been called more than once, will throw exception.
         /// </remarks>
+        public static ContainerBuilder AddUnitOfWork<TContext>(this ContainerBuilder builder)
+        {
+            builder.RegisterType<TContext>().InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(UnitOfWork<>)).As(typeof(IUnitOfWork<>)).InstancePerLifetimeScope();
+            return builder;
+        }
+        /// <summary>
+        /// Registers the unit of work given context as a service in the <see cref="ContainerBuilder"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="ContainerBuilder"/> to add services to.</param>
+        /// <returns>The same service collection so that multiple calls can be chained.</returns>
+        /// <remarks>
+        /// </remarks>
         public static ContainerBuilder AddUnitOfWork(this ContainerBuilder builder)
         {
             builder.RegisterGeneric(typeof(UnitOfWork<>)).As(typeof(IUnitOfWork<>)).InstancePerLifetimeScope();
