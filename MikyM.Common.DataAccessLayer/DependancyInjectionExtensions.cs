c0a066d2b83610248ca843ae018c41a9fd6a363c
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2021-10-18 10:34:23 +0200

    feat: DI extensions

---
+++ MikyM.Common.DataAccessLayer/DependancyInjectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿// This file is part of MikyM.Common.DataAccessLayer project
+//
+// Copyright (C) 2021 Krzysztof Kupisz - MikyM
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+using MikyM.Common.DataAccessLayer.Repositories;
+using MikyM.Common.DataAccessLayer.UnitOfWork;
+
+namespace MikyM.Common.DataAccessLayer
+{
+    public static class DependancyInjectionExtensions
+    {
+        public static void AddDataAccessLayer(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IReadOnlyRepository<>), typeof(ReadOnlyRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+        }
+        
+        public static void AddDataAccessLayer(this ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(ReadOnlyRepository<>)).As(typeof(IReadOnlyRepository<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>))
+                .InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(UnitOfWork<>)).As(typeof(IUnitOfWork<>)).InstancePerLifetimeScope();
+        }
+    }
+}
